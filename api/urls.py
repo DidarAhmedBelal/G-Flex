c440420f9e450d397d8fffc86cb909d89b39d4a0
Didar Ahmed Belal <belalislam294@gmail.com>
2025-07-12 01:52:33 -0700

    sdfjdfkld

--- api/urls.py
+++ api/urls.py
@@ -1,23 +1,21 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
+from django.urls import path
+from users.views import UserList
 from users.views import (
-    UserList,
-    SignupView,
-    MyProfileView,
-    LoginView,
-    ChangePasswordView,
-    VerifyOTPView,
-    SendOTPView,
+    LoginView,  SignupView, MyProfileView,
+    ChangePasswordView, VerifyOTPView, SendOTPView
     )
 
-urlpatterns = [
-    
-path('login/', LoginView.as_view(), name='login'),
-path('signup/', SignupView.as_view(), name='signup'),
-path('profile/', MyProfileView.as_view(), name='my-profile'),
-path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
-path('send-otp/', SendOTPView.as_view(), name='send-otp'),
-path('users/', UserList.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+router = DefaultRouter()
+router.register('users', UserList, basename='user')
 
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('profile/', MyProfileView.as_view(), name='my-profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
+    path('send-otp/', SendOTPView.as_view(), name='send-otp'),
 ]
+
+urlpatterns += router.urls
--- users/models.py
+++ users/models.py
@@ -6,11 +6,12 @@ def user_profile_upload_path(instance, filename):
     return f"profile_pics/user_{instance.id}/{filename}"
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=False, blank=True, null=True)
+    username = None 
     first_name = models.CharField(max_length=10, blank=True, null=True)
     last_name = models.CharField(max_length=10, blank=True, null=True)
 
-    email = models.EmailField(unique=True, blank=False)
+    email = models.EmailField(unique=True) 
+
     otp = models.CharField(max_length=6, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     otp_created_at = models.DateTimeField(null=True, blank=True, default=timezone.now)
@@ -24,7 +25,7 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []  
 
     def __str__(self):
         return self.email
